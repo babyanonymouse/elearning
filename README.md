71a58c6f17cb788acfcb2ca79956c2fd756d8f38
babyanonymouse <babyanonymouse2@gmail.com>
2024-10-15 19:40:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Elearning Web
+
+## React + Vite Project
+
+- Inspiration: The coding jouney
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero/Hero.jsx"
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden bg-white text-dark">
+      <Hero />
+    </main>
+  )
+}
+
+export default App

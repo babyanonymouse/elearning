e673773dc691f43f3c0d68eb25fe98500710580b
babyanonymouse <babyanonymouse2@gmail.com>
2024-10-17 13:20:12 +0300

    banner done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero/Hero.jsx"
 import Services from "./components/Services/Services"
+import Banner from "./components/Banner/Banner"
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
       <Services />
+      <Banner />
     </main>
   )
 }

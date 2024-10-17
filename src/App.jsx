4257a3df4da116364c71c1b461454b7d0bbf32a4
babyanonymouse <babyanonymouse2@gmail.com>
2024-10-17 14:27:26 +0300

    join community section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero/Hero.jsx"
 import Services from "./components/Services/Services"
 import Banner from "./components/Banner/Banner"
 import Subscribe from "./components/Subscribe/Subscribe"
+import Banner2 from "./components/Banner/Banner2.jsx"
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
@@ -9,6 +10,7 @@ const App = () => {
       <Services />
       <Banner />
       <Subscribe />
+      <Banner2 />
     </main>
   )
 }

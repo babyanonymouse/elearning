fb8e1abe58a07e458ea0c46e5f5ff151d5a3e215
babyanonymouse <babyanonymouse2@gmail.com>
2024-10-17 14:07:56 +0300

    subscribe section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./components/Hero/Hero.jsx"
 import Services from "./components/Services/Services"
 import Banner from "./components/Banner/Banner"
+import Subscribe from "./components/Subscribe/Subscribe"
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
       <Services />
       <Banner />
+      <Subscribe />
     </main>
   )
 }
